d26f4031c824e6cc24587b8a122773cbce8d1d46
ViniEgsp <contaderegistro.1@gmail.com>
2023-05-07 10:18:41 -0300

    hhhh

---
+++ PHP sem medo/Estaticas/Margin e Padding/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Margin e Padding</title>
+</head>
+<body>
+    <a href="https://youtu.be/5VqeHRrJhpg?list=PLhUlhohzo17rUo1Rm2IT77HZUaMI5VtAH">Link da Aula</a>
+</body>
+</html>
